39ebf604b6c4fff47bbf4f358a211286420706c3
Carter MacLennan <carter.maclennan@verosource.com>
2020-11-17 22:55:22 -0400

    Created node server & added standard template code

---
+++ app.js
@@ -0,0 +1,15 @@
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const express = require("express");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(3000, function() {
+  console.log("Server running on port 3000...");
+});
